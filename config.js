729888e679b9085429f13a09996ba06daab337f3
Alex Tiley <alextiley1@gmail.com>
2019-11-27 14:32:43 +0200

    feat: added analtyics
    * added google analytics
    * updated app icon
    * committed public dir to repo
    * updated `.gitignore`

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteTitle: 'Alex Tiley', // <title>
-  manifestName: 'Highlights',
-  manifestShortName: 'Landing', // max 12 characters
+  manifestName: 'AlexTiley',
+  manifestShortName: 'AT', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
+  manifestBackgroundColor: '##345eeb',
+  manifestThemeColor: '##345eeb',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/img/atIcon.png',
   heading: 'Alex Tiley',
   subHeading: 'Web & Mobile Developer',
   // social
@@ -14,27 +14,27 @@ module.exports = {
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/alextiley1'
+      url: 'https://github.com/alextiley1',
     },
     {
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/AlexTiley1'
+      url: 'https://twitter.com/AlexTiley1',
     },
     {
       icon: 'fa-facebook',
       name: 'Facebook',
-      url: 'https://www.facebook.com/alextiley1'
+      url: 'https://www.facebook.com/alextiley1',
     },
     {
       icon: 'fa-linkedin',
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/alextiley1/'
+      url: 'https://www.linkedin.com/in/alextiley1/',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:alextiley1@gmail.com'
-    }
-  ]
-}
+      url: 'mailto:alextiley1@gmail.com',
+    },
+  ],
+};
